459e80d16110ea2d87026dff531fe6918fdeb0c9
Julien Nicoulaud <julien.nicoulaud@gmail.com>
2010-12-12 11:05:27 +0100

    Add an archetype with release management, based on android-with-test

---
+++ android-full/src/main/resources/archetype-resources/application-it/src/main/java/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package ${package}.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ${package}.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class);
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
